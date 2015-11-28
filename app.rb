a4733a47b1061cc2597fe3e7f27dc12420cda96a
Savant Krishna <mail@sksavant.me>
2015-11-28 12:15:31 -0500

    almost done collating api

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require 'sinatra'
 configure do
   set :base_url, "http://cdn.newsapi.com.au/content/v2/"
   set :api_key, "f2qtvndyfh4ensub44utdwtf"
+  set :rel_keys, ["title", "id", "subtitle", "description", "authors", "body"]
   enable :sessions
   #use Rack::SSL
 end
@@ -32,7 +33,7 @@ def collection_url(id, num=20)
 end
 
 get '/test' do
-  @news_id = "3de382308aa8e8f054c3085205136344"
+  @news_id = "ad8dd851fc70756ef81bc81acfdb8ae5"
   response = HTTParty.get(story_url(@news_id))
   output = JSON.parse(response.body)
   erb 'News title: ' << output["title"] << "</br>" << 'Body : ' << output["body"]
@@ -54,8 +55,18 @@ get '/test/firehose' do
 
 end
 
+get '/story.json' do
+  content_type :json, 'charset' => 'utf-8'
+  news_id = "ad8dd851fc70756ef81bc81acfdb8ae5"
+  news_id = params["id"] if params["id"]
+  response = HTTParty.get(story_url(news_id))
+  output = JSON.parse(response.body).to_hash
+  output.delete_if { |k,v| !settings.rel_keys.include? k }.to_json
+end
+
+
 get '/firehose.json' do
-  content_type :json
+  content_type :json, 'charset' => 'utf-8'
   num = 100
   num = params["num"].to_i if params["num"]
   response = HTTParty.get(firehose_url(num))
@@ -63,15 +74,15 @@ get '/firehose.json' do
   output = JSON.parse(response.body)
   j = []
   output["results"].each do |r|
-    j << {:title => r["title"], :authors => r["authors"], :body => r["body"]}
+    j << r.delete_if {|k,v| !settings.rel_keys.include? k }
   end
   j.to_json
 end
 
 get '/collection.json' do
-  content_type :json
+  content_type :json, 'charset' => 'utf-8'
   col_id = "517ed3c64dda2f97fa940ad29b73ba4c"
-  col_id = params["col"] if params["col"]
+  col_id = params["id"] if params["id"]
   num = 20
   num = params["num"].to_i if params["num"]
   response = HTTParty.get(collection_url(col_id, num))
@@ -80,7 +91,7 @@ get '/collection.json' do
   output = JSON.parse(response.body)
   j = []
   output["results"].each do |r|
-    j << {:title => r["title"], :authors => r["authors"], :body => r["body"]}
+    j << r.delete_if {|k,v| !settings.rel_keys.include? k }
   end
   j.to_json
 end
